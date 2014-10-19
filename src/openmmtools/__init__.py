c344c631b1f124b4755d0b4beb6e146fae647e20
John Chodera (MSKCC) <choderaj@mskcc.org>
2014-10-19 16:49:01 -0400

    Hoping I've fixed version.py

--- src/openmmtools/__init__.py
+++ src/openmmtools/__init__.py
@@ -6,8 +6,8 @@ Various Python utilities for OpenMM.
 """
 
 # Define global version.
-from openmmtools import version
+import version
 __version__ = version.version
 
 # Import modules.
-from openmmtools import testsystems
+import testsystems
