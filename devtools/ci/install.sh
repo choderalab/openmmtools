b44d3b840a6b179bd0b2c80ced2c229021b82d07
John Chodera (MSKCC) <choderaj@mskcc.org>
2014-10-18 23:56:27 -0400

    Fixed some path names.

--- devtools/ci/install.sh
+++ devtools/ci/install.sh
@@ -8,6 +8,6 @@ export PATH=$HOME/miniconda/bin:$PATH
 
 conda update --yes conda
 conda config --add channels http://conda.binstar.org/omnia
-conda create --yes -n ${python} python=${python} --file tools/ci/requirements-conda.txt
+conda create --yes -n ${python} python=${python} --file devtools/ci/requirements-conda.txt
 source activate $python
 $HOME/miniconda/envs/${python}/bin/pip install $PIP_ARGS nose-exclude
