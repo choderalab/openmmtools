292c10c178c9bb3b78e031bbf9bbb4ca51aa70a8
John Chodera (MSKCC) <choderaj@mskcc.org>
2015-01-13 00:19:55 -0500

    Re-enabled after_success script with openmmtools-dev package name.

--- devtools/ci/after_sucess.sh
+++ devtools/ci/after_sucess.sh
@@ -11,8 +11,9 @@ fi
 
 
 if [[ "2.7 3.3 3.4" =~ "$python" ]]; then
-    conda install --yes binstar
-    binstar -t $BINSTAR_TOKEN upload --force -u omnia -p openmmtools $HOME/miniconda/conda-bld/linux-64/${PACKAGENAME}-*
+    conda install --yes --quiet binstar
+    BINSTAR_VERSION=`git describe 2> /dev/null || git rev-parse --short HEAD` # From http://wygoda.net/blog/getting-useful-git-revision-information/
+    binstar -t $BINSTAR_TOKEN upload -v $BINSTAR_VERSION -u omnia -p openmmtools-dev $HOME/miniconda/conda-bld/linux-64/${PACKAGENAME}-*
 fi
 
 if [[ "$python" != "2.7" ]]; then
