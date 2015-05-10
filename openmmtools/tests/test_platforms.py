73e3882cec5fdf8cbabe6997d63e0931b49be6ae
John Chodera (MSKCC) <choderaj@mskcc.org>
2015-05-10 14:24:37 -0400

    Added nosetests for platform test.

---
+++ openmmtools/tests/test_platforms.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+import os, os.path
+import logging
+
+def test_openmm_platforms():
+    """Testing comparison of platforms.
+    """
+    from openmmtools.scripts import test_openmm_platforms
+    test_openmm_platforms.main()
