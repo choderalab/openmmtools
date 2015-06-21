49109fcda48122bc8d3543da8018780f329d7cb9
John Chodera (MSKCC) <choderaj@mskcc.org>
2015-06-21 11:26:11 -0400

    Fix conda recipe for openmmtools-dev

--- devtools/conda-recipe/build.sh
+++ devtools/conda-recipe/build.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-cp -r $RECIPE_DIR/../.. $SRC_DIR
-$PYTHON setup.py clean
 $PYTHON setup.py install
 
 # Copy examples
 # TODO: Have setup.py install examples instead?
 mkdir $PREFIX/share/openmmtools
-cp -r $RECIPE_DIR/../../examples $PREFIX/share/openmmtools/
+cp -r examples $PREFIX/share/openmmtools/
