69228d034ee48838cda963a151f4cdf4f10457df
John Chodera (MSKCC) <choderaj@mskcc.org>
2015-10-25 18:01:11 -0700

    Increment version to 0.7.2

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ import subprocess
 DOCLINES = __doc__.split("\n")
 
 ########################
-VERSION = "0.7.1"
+VERSION = "0.7.2"
 ISRELEASED = False
 __version__ = VERSION
 ########################
