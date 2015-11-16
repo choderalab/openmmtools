92b0b235f862c0728b0c703e607be3663e19df23
John Chodera <choderaj@mskcc.org>
2015-11-15 21:25:56 -0500

    Update test with timing test (not a real test yet).

--- openmmtools/tests/test_integrators.py
+++ openmmtools/tests/test_integrators.py
@@ -14,7 +14,7 @@ Test custom integrators.
 #=============================================================================================
 
 import re
-import numpy
+import numpy as np
 
 from simtk import unit
 from simtk import openmm
@@ -58,7 +58,7 @@ def check_stability(integrator, test, platform=None, nsteps=100, temperature=300
    # Check that simulation has not exploded.
    state = context.getState(getEnergy=True)
    potential = state.getPotentialEnergy() / kT
-   if numpy.isnan(potential):
+   if np.isnan(potential):
       raise Exception("Potential energy for integrator %s became NaN." % integrator.__doc__)
 
    del context
@@ -113,9 +113,25 @@ def test_integrator_decorators():
     assert integrator.acceptance_rate == 1.0
 
 def test_fire_minimization():
-    integrator = integrators.FIREMinimizationIntegrator()
-    testsystem = testsystems.LennardJonesFluid()
-    nsteps = 100
+    tolerance = 10.0 * unit.kilojoules_per_mole / unit.nanometer
+
+    # Disorder the system
+    testsystem = testsystems.DHFRExplicit()
+    nparticles = testsystem.system.getNumParticles()
+    temperature = 300 * unit.kelvin
+    collision_rate = 90.0 / unit.picosecond
+    timestep = 2.0 * unit.femtosecond
+    nsteps_equil = 2500
+    integrator = openmm.LangevinIntegrator(temperature, collision_rate, timestep)
+    context = openmm.Context(testsystem.system, integrator)
+    context.setPositions(testsystem.positions)
+    integrator.step(nsteps_equil)
+    testsystem.positions = context.getState(getPositions=True).getPositions(asNumpy=True)
+    del context, integrator
+
+    # Minimize.
+    integrator = integrators.FIREMinimizationIntegrator(tolerance=tolerance)
+    maxits = 100
 
     context = openmm.Context(testsystem.system, integrator)
     context.setPositions(testsystem.positions)
@@ -123,11 +139,37 @@ def test_fire_minimization():
 
     initial_energy = context.getState(getEnergy=True).getPotentialEnergy() / unit.kilocalories_per_mole
 
-    integrator.step(nsteps)
+    import time
+    initial_time = time.time()
+    integrator.step(maxits)
+    final_time = time.time()
+    elapsed_time = final_time - initial_time
 
-    final_energy = context.getState(getEnergy=True).getPotentialEnergy() / unit.kilocalories_per_mole
+    final_energy = context.getState(getEnergy=True).getPotentialEnergy() / unit.kilojoules_per_mole
+    final_gnorm = np.sqrt( ((context.getState(getForces=True).getForces(asNumpy=True) / (unit.kilojoules_per_mole/unit.nanometer))**2).sum() / nparticles)
 
-    print "FIRE minimizer"
-    print "Initial energy: %12.3f kcal/mol" % initial_energy
-    print "Final energy:   %12.3f kcal/mol" % final_energy
+    print "FIRE minimizer (%.3f s)" % elapsed_time
+    print "Initial energy: %12.3f kJ/mol" % initial_energy
+    print "Final energy:   %12.3f kJ/mol" % final_energy
+    print "Final gnorm:    %12.3f kJ/mol/nm" % final_gnorm
 
+    # Reset
+    context.setPositions(testsystem.positions)
+    initial_time = time.time()
+    openmm.LocalEnergyMinimizer.minimize(context, tolerance, maxits)
+    final_time = time.time()
+    elapsed_time = final_time - initial_time
+
+    final_energy = context.getState(getEnergy=True).getPotentialEnergy() / unit.kilojoules_per_mole
+    final_gnorm = np.sqrt( ((context.getState(getForces=True).getForces(asNumpy=True) / (unit.kilojoules_per_mole/unit.nanometer))**2).sum() / nparticles)
+
+    print "LocalEnergyMinimizer (%.3f s)" % elapsed_time
+    print "Initial energy: %12.3f kJ/mol" % initial_energy
+    print "Final energy:   %12.3f kJ/mol" % final_energy
+    print "Final gnorm:    %12.3f kJ/mol/nm" % final_gnorm
+
+    del context, integrator
+
+    
+if __name__ == '__main__':
+   test_fire_minimization()
