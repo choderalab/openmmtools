537a9bc6877b0a87f6c236eb4831a8607937770f
jchodera <john.chodera@choderalab.org>
2016-08-04 12:36:59 -0400

    Bugfix for HostGuestExplicit arguments

--- openmmtools/testsystems.py
+++ openmmtools/testsystems.py
@@ -3192,7 +3192,7 @@ class HostGuestExplicit(TestSystem):
     >>> (system, positions) = testsystem.system, testsystem.positions
     """
 
-    def __init__(self, constraints=app.HBonds, rigid_water=True, nonbondedCutoff=9.0*unit.angstroms, use_dispersion_correction=True, nonbondedMethod=app.PME, hydrogenMass=None, switch_width=1.5*angstroms, ewaldErrorTolerance=1.0e-6, **kwargs):
+    def __init__(self, constraints=app.HBonds, rigid_water=True, nonbondedCutoff=9.0*unit.angstroms, use_dispersion_correction=True, nonbondedMethod=app.PME, hydrogenMass=None, switch_width=1.5*unit.angstroms, ewaldErrorTolerance=1.0e-6, **kwargs):
 
         TestSystem.__init__(self, **kwargs)
 
