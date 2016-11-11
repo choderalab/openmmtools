ae3f0a3021eabf7deb7bbc83458099329dac0a70
jchodera <john.chodera@choderalab.org>
2016-11-11 17:29:58 -0500

    Fix travis install script

--- devtools/travis-ci/install.sh
+++ devtools/travis-ci/install.sh
@@ -1,6 +1,6 @@
 MINICONDA=Miniconda2-latest-Linux-x86_64.sh
 MINICONDA_MD5=$(curl -s http://repo.continuum.io/miniconda/ | grep -A3 $MINICONDA | sed -n '4p' | sed -n 's/ *<td>\(.*\)<\/td> */\1/p')
-wget http://repo.continuum.io/miniconda/$MINICONDA
+wget https://repo.continuum.io/miniconda/$MINICONDA
 if [[ $MINICONDA_MD5 != $(md5sum $MINICONDA | cut -d ' ' -f 1) ]]; then
     echo "Miniconda MD5 mismatch"
     exit 1
