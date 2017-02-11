196e448e2bf401d517e8b39e0ecbb0b2cfd6318d
Andrea Rizzi <andrrizzi@gmail.com>
2017-02-11 18:39:17 -0500

    Changed 3-space tabs to 4-space tabs

--- openmmtools/tests/test_integrators.py
+++ openmmtools/tests/test_integrators.py
@@ -59,41 +59,41 @@ def get_all_custom_integrators(only_thermostated=False):
 
 
 def check_stability(integrator, test, platform=None, nsteps=100, temperature=300.0*unit.kelvin):
-   """
-   Check that the simulation does not explode over a number integration steps.
+    """
+    Check that the simulation does not explode over a number integration steps.
 
-   Parameters
-   ----------
-   integrator : simtk.openmm.Integrator
-      The integrator to test.
-   test : testsystem
-      The testsystem to test.
+    Parameters
+    ----------
+    integrator : simtk.openmm.Integrator
+       The integrator to test.
+    test : testsystem
+       The testsystem to test.
 
-   """
-   kT = kB * temperature
+    """
+    kT = kB * temperature
 
-   # Create Context and initialize positions.
-   if platform:
-      context = openmm.Context(test.system, integrator, platform)
-   else:
-      context = openmm.Context(test.system, integrator)
-   context.setPositions(test.positions)
-   context.setVelocitiesToTemperature(temperature) # TODO: Make deterministic.
- 
-   # Set integrator temperature
-   if hasattr(integrator, 'setTemperature'):
-      integrator.setTemperature(temperature)
+    # Create Context and initialize positions.
+    if platform:
+        context = openmm.Context(test.system, integrator, platform)
+    else:
+        context = openmm.Context(test.system, integrator)
+    context.setPositions(test.positions)
+    context.setVelocitiesToTemperature(temperature) # TODO: Make deterministic.
 
-   # Take a number of steps.
-   integrator.step(nsteps)
+    # Set integrator temperature
+    if hasattr(integrator, 'setTemperature'):
+        integrator.setTemperature(temperature)
 
-   # Check that simulation has not exploded.
-   state = context.getState(getEnergy=True)
-   potential = state.getPotentialEnergy() / kT
-   if numpy.isnan(potential):
-      raise Exception("Potential energy for integrator %s became NaN." % integrator.__doc__)
+    # Take a number of steps.
+    integrator.step(nsteps)
 
-   del context
+    # Check that simulation has not exploded.
+    state = context.getState(getEnergy=True)
+    potential = state.getPotentialEnergy() / kT
+    if numpy.isnan(potential):
+        raise Exception("Potential energy for integrator %s became NaN." % integrator.__doc__)
+
+    del context
 
 
 def check_integrator_temperature(integrator, temperature, has_changed):
@@ -142,28 +142,29 @@ def check_integrator_temperature_getter_setter(integrator):
 #=============================================================================================
 
 def test_stabilities():
-   """
-   Test integrators for stability over a short number of steps.
+    """
+    Test integrators for stability over a short number of steps.
 
-   """
-   ts = testsystems  # shortcut
-   test_cases = {'harmonic oscillator': ts.HarmonicOscillator(),
-                 'alanine dipeptide in implicit solvent': ts.AlanineDipeptideImplicit()}
-   custom_integrators = get_all_custom_integrators()
+    """
+    ts = testsystems  # shortcut
+    test_cases = {'harmonic oscillator': ts.HarmonicOscillator(),
+                  'alanine dipeptide in implicit solvent': ts.AlanineDipeptideImplicit()}
+    custom_integrators = get_all_custom_integrators()
 
-   for test_name, test in test_cases.items():
-      for integrator_name, integrator_class in custom_integrators:
-         integrator = integrator_class()
-         integrator.__doc__ = integrator_name
-         check_stability.description = ("Testing {} for stability over a short number of "
-                                        "integration steps of a {}.").format(integrator_name, test_name)
-         yield check_stability, integrator, test
+    for test_name, test in test_cases.items():
+        for integrator_name, integrator_class in custom_integrators:
+            integrator = integrator_class()
+            integrator.__doc__ = integrator_name
+            check_stability.description = ("Testing {} for stability over a short number of "
+                                           "integration steps of a {}.").format(integrator_name, test_name)
+            yield check_stability, integrator, test
+
 
 def test_integrator_decorators():
     integrator = integrators.HMCIntegrator(timestep=0.05 * unit.femtoseconds)
     testsystem = testsystems.IdealGas()
     nsteps = 25
-    
+
     context = openmm.Context(testsystem.system, integrator)
     context.setPositions(testsystem.positions)
     context.setVelocitiesToTemperature(300 * unit.kelvin)
@@ -174,34 +175,35 @@ def test_integrator_decorators():
     assert integrator.n_trials == nsteps
     assert integrator.acceptance_rate == 1.0
 
+
 def test_vvvr_shadow_work_accumulation():
-   ''' When `monitor_work==True`, assert that global `shadow_work` is initialized to zero and
-   reaches a nonzero value after integrating a few dozen steps.
-   
-   By default (`monitor_work=False`), assert that there is no global name for `shadow_work`. '''
-   
-   # test `monitor_work=True` --> accumulation of a nonzero value in global `shadow_work`
-   testsystem = testsystems.HarmonicOscillator()
-   system, topology = testsystem.system, testsystem.topology
-   temperature = 298.0 * unit.kelvin
-   integrator = integrators.VVVRIntegrator(temperature, monitor_work=True)
-   context = openmm.Context(system, integrator)
-   context.setPositions(testsystem.positions)
-   context.setVelocitiesToTemperature(temperature)
-   assert(integrator.getGlobalVariableByName('shadow_work') == 0)
-   integrator.step(25)
-   assert(integrator.getGlobalVariableByName('shadow_work') != 0)
-   
-   # test default (`monitor_work=False`, `monitor_heat=False`) --> absence of a global `shadow_work`
-   integrator = integrators.VVVRIntegrator(temperature)
-   context = openmm.Context(system, integrator)
-   context.setPositions(testsystem.positions)
-   context.setVelocitiesToTemperature(temperature)
-   integrator.step(25)
-   # get the names of all global variables
-   n_globals = integrator.getNumGlobalVariables()
-   names_of_globals = [integrator.getGlobalVariableName(i) for i in range(n_globals)]
-   assert('shadow_work' not in names_of_globals)
+    """When `monitor_work==True`, assert that global `shadow_work` is initialized to zero and
+    reaches a nonzero value after integrating a few dozen steps.
+
+    By default (`monitor_work=False`), assert that there is no global name for `shadow_work`."""
+
+    # test `monitor_work=True` --> accumulation of a nonzero value in global `shadow_work`
+    testsystem = testsystems.HarmonicOscillator()
+    system, topology = testsystem.system, testsystem.topology
+    temperature = 298.0 * unit.kelvin
+    integrator = integrators.VVVRIntegrator(temperature, monitor_work=True)
+    context = openmm.Context(system, integrator)
+    context.setPositions(testsystem.positions)
+    context.setVelocitiesToTemperature(temperature)
+    assert(integrator.getGlobalVariableByName('shadow_work') == 0)
+    integrator.step(25)
+    assert(integrator.getGlobalVariableByName('shadow_work') != 0)
+
+    # test default (`monitor_work=False`, `monitor_heat=False`) --> absence of a global `shadow_work`
+    integrator = integrators.VVVRIntegrator(temperature)
+    context = openmm.Context(system, integrator)
+    context.setPositions(testsystem.positions)
+    context.setVelocitiesToTemperature(temperature)
+    integrator.step(25)
+    # get the names of all global variables
+    n_globals = integrator.getNumGlobalVariables()
+    names_of_globals = [integrator.getGlobalVariableName(i) for i in range(n_globals)]
+    assert('shadow_work' not in names_of_globals)
 
 
 def test_temperature_getter_setter():
--- openmmtools/tests/test_integrators_and_testsystems.py
+++ openmmtools/tests/test_integrators_and_testsystems.py
@@ -31,70 +31,70 @@ kB = unit.BOLTZMANN_CONSTANT_kB * unit.AVOGADRO_CONSTANT_NA
 #=============================================================================================
 
 def check_combination(integrator, test, platform=None):
-   """
-   Check combination of integrator and testsystem.
+    """
+    Check combination of integrator and testsystem.
 
-   Parameters
-   ----------
-   integrator : simtk.openmm.Integrator
-      The integrator to test.
-   test : testsystem
-      The testsystem to test.
+    Parameters
+    ----------
+    integrator : simtk.openmm.Integrator
+       The integrator to test.
+    test : testsystem
+       The testsystem to test.
 
-   """
+    """
 
-   # Create Context and initialize positions.
-   if platform:
-      context = openmm.Context(test.system, integrator, platform)
-   else:
-      context = openmm.Context(test.system, integrator)
+    # Create Context and initialize positions.
+    if platform:
+        context = openmm.Context(test.system, integrator, platform)
+    else:
+        context = openmm.Context(test.system, integrator)
 
-   # Clean up.
-   del context
+    # Clean up.
+    del context
 
-   return
+    return
+
 
 #=============================================================================================
 # TESTS
 #=============================================================================================
 
 def test_integrators_and_testsystems():
-   """
-   Test combinations of integrators and testsystems to ensure there are no global context parameters.
+    """
+    Test combinations of integrators and testsystems to ensure there are no global context parameters.
 
-   """
-   from openmmtools import integrators, testsystems
+    """
+    from openmmtools import integrators, testsystems
 
-   # Get all the CustomIntegrators in the integrators module.
-   is_integrator = lambda x: (inspect.isclass(x) and
-                              issubclass(x, openmm.CustomIntegrator) and
-                              x != integrators.ThermostatedIntegrator)
-   custom_integrators = inspect.getmembers(integrators, predicate=is_integrator)
+    # Get all the CustomIntegrators in the integrators module.
+    is_integrator = lambda x: (inspect.isclass(x) and
+                               issubclass(x, openmm.CustomIntegrator) and
+                               x != integrators.ThermostatedIntegrator)
+    custom_integrators = inspect.getmembers(integrators, predicate=is_integrator)
 
-   def all_subclasses(cls):
-       """Return list of all subclasses and subsubclasses for a given class."""
-       return cls.__subclasses__() + [s for s in cls.__subclasses__()]
-   testsystem_classes = all_subclasses(testsystems.TestSystem)
-   testsystem_names = [ cls.__name__ for cls in testsystem_classes ]
+    def all_subclasses(cls):
+        """Return list of all subclasses and subsubclasses for a given class."""
+        return cls.__subclasses__() + [s for s in cls.__subclasses__()]
+    testsystem_classes = all_subclasses(testsystems.TestSystem)
+    testsystem_names = [ cls.__name__ for cls in testsystem_classes ]
 
-   # Use Reference platform.
-   platform = openmm.Platform.getPlatformByName('Reference')
+    # Use Reference platform.
+    platform = openmm.Platform.getPlatformByName('Reference')
 
-   for testsystem_name in testsystem_names:
-      # Create testsystem.
-      try:
-          testsystem = getattr(testsystems, testsystem_name)()
-      except ImportError as e:
-          print(e)
-          print("Skipping %s due to missing dependency" % testsystem_name)
-          continue
+    for testsystem_name in testsystem_names:
+        # Create testsystem.
+        try:
+            testsystem = getattr(testsystems, testsystem_name)()
+        except ImportError as e:
+            print(e)
+            print("Skipping %s due to missing dependency" % testsystem_name)
+            continue
 
-      for integrator_name, integrator_class in custom_integrators:
-         # Create integrator.
-         integrator = integrator_class()
+        for integrator_name, integrator_class in custom_integrators:
+            # Create integrator.
+            integrator = integrator_class()
 
-         # Create test.
-         f = partial(check_combination, integrator, testsystem, platform)
-         f.description = "Checking combination of %s and %s" % (integrator_name, testsystem_name)
-         yield f
-
+            # Create test.
+            f = partial(check_combination, integrator, testsystem, platform)
+            f.description = "Checking combination of %s and %s" % (integrator_name, testsystem_name)
+            yield f
