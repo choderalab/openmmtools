62f6acab560d8c1fbf84e6d8e5fa643fd4d856d5
jchodera <john.chodera@choderalab.org>
2017-02-15 14:17:53 -0500

    Get login credentials for RabbitMQ from environment variables

--- examples/distributed/README.md
+++ examples/distributed/README.md
@@ -2,6 +2,15 @@
 
 See [First Steps with Celery](http://docs.celeryproject.org/en/latest/getting-started/first-steps-with-celery.html).
 
+First, set your environment variables:
+```
+RABBITMQ_SERVER
+RABBITMQ_USERNAME
+RABBITMQ_PASSWORD
+RABBITMQ_PORT (defaults to '5762')
+RABBITMQ_VHOST (defaults to 'celery')
+```
+
 To run, first start one or more workers:
 ```bash
 $ celery -A openmmtools.distributed worker -l info
--- openmmtools/distributed/celery.py
+++ openmmtools/distributed/celery.py
@@ -1,9 +1,19 @@
 from __future__ import absolute_import, unicode_literals
 from celery import Celery
+import os
+
+server = os.environ['RABBITMQ_SERVER']
+username = os.environ['RABBITMQ_USERNAME']
+password = os.environ['RABBITMQ_PASSWORD']
+port = os.getenv('RABBITMQ_PORT', '5762')
+vhost = os.getenv('RABBITMQ_VHOST', 'celery')
+
+broker = 'amqp://%(username)s:%(password)s@%(server)s:%(port)s/%(vhost)s' % vars()
+backend = 'amqp://%(username)s:%(password)s@%(server)s:%(port)s/%(vhost)s' % vars()
 
 app = Celery('openmmtools.distributed',
-             broker='amqp://',
-             backend='amqp://',
+             broker=broker,
+             backend=backend,
              include=['openmmtools.distributed.tasks'])
 
 # Optional configuration, see the application user guide.
