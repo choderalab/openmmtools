cc6a5473aff2c074413da9451dd1a59ade44ad03
Andrea Rizzi <andrrizzi@gmail.com>
2017-03-06 16:51:25 -0500

    Changed formatting and order alchemical region parameters

--- openmmtools/alchemy.py
+++ openmmtools/alchemy.py
@@ -566,13 +566,16 @@ class AlchemicalState(object):
 # ALCHEMICAL REGION
 # =============================================================================
 
-_ALCHEMICAL_REGION_ARGS = collections.OrderedDict(
-    [('alchemical_atoms', None), ('alchemical_bonds', None),
-     ('alchemical_angles', None), ('alchemical_torsions', None),
-     ('annihilate_electrostatics', True), ('annihilate_sterics', False),
-     ('softcore_alpha', 0.5), ('softcore_beta', 0.0), ('softcore_a', 1), ('softcore_b', 1),
-     ('softcore_c', 6), ('softcore_d', 1), ('softcore_e', 1), ('softcore_f', 2)]
-)
+_ALCHEMICAL_REGION_ARGS = collections.OrderedDict([
+    ('alchemical_atoms', None),
+    ('alchemical_bonds', None),
+    ('alchemical_angles', None),
+    ('alchemical_torsions', None),
+    ('annihilate_electrostatics', True),
+    ('annihilate_sterics', False),
+    ('softcore_alpha', 0.5), ('softcore_a', 1), ('softcore_b', 1), ('softcore_c', 6),
+    ('softcore_beta', 0.0), ('softcore_d', 1), ('softcore_e', 1), ('softcore_f', 2)
+])
 
 
 # The class is just a way to document the namedtuple.
@@ -603,19 +606,20 @@ class AlchemicalRegion(collections.namedtuple('AlchemicalRegion', _ALCHEMICAL_RE
         entries are softened with 'lambda_torsions'. If set to True, this list
         is auto-generated to include al proper torsions involving any alchemical
         atoms. Improper torsions are not softened (default is None).
-    annihilate_electrostatics : bool
-        If True, electrostatics should be annihilated, rather than decoupled.
-    annihilate_sterics : bool
+    annihilate_electrostatics : bool, optional
+        If True, electrostatics should be annihilated, rather than decoupled
+        (default is True).
+    annihilate_sterics : bool, optional
         If True, sterics (Lennard-Jones or Halgren potential) will be annihilated,
-        rather than decoupled.
+        rather than decoupled (default is False).
     softcore_alpha : float, optional
         Alchemical softcore parameter for Lennard-Jones (default is 0.5).
-    softcore_beta : float, optional
-        Alchemical softcore parameter for electrostatics. Set this to zero
-        to recover standard electrostatic scaling (default is 0.0).
     softcore_a, softcore_b, softcore_c : float, optional
         Parameters modifying softcore Lennard-Jones form. Introduced in
         Eq. 13 of Ref. [1] (default is 1).
+    softcore_beta : float, optional
+        Alchemical softcore parameter for electrostatics. Set this to zero
+        to recover standard electrostatic scaling (default is 0.0).
     softcore_d, softcore_e, softcore_f : float, optional
         Parameters modifying softcore electrostatics form (default is 1).
 
