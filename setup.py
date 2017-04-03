d2f33f4641edb9d7b2072eb317ea6b8aac6af513
Andrea Rizzi <andrrizzi@gmail.com>
2017-04-03 19:49:11 -0400

    Set ISRELEASED to False and bump to 0.9.1 after releasing 0.9.0

--- setup.py
+++ setup.py
@@ -14,8 +14,8 @@ import subprocess
 DOCLINES = __doc__.split("\n")
 
 ########################
-VERSION = "0.9.0"
-ISRELEASED = True
+VERSION = "0.9.1"
+ISRELEASED = False
 __version__ = VERSION
 ########################
 CLASSIFIERS = """\
