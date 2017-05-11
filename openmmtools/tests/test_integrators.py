93d43f3fc7a47712f62d9b394e50b0aeb0ba5aca
Gregory Ross <gregoryross.uk@gmail.com>
2017-05-11 17:08:01 -0400

    Reduced the number of platforms used in TestExternalPerturbationLangevinIntegrator.test_protocol_work_accumulation_waterbox in order to speed up unit tests.

--- openmmtools/tests/test_integrators.py
+++ openmmtools/tests/test_integrators.py
@@ -489,12 +489,12 @@ class TestExternalPerturbationLangevinIntegrator(TestCase):
         parameter_name = 'lambda_electrostatics'
         parameter_initial = 1.0
         parameter_final = 0.0
-        platform_names = [ openmm.Platform.getPlatform(index).getName() for index in range(openmm.Platform.getNumPlatforms()) ]
+        #platform_names = [ openmm.Platform.getPlatform(index).getName() for index in range(openmm.Platform.getNumPlatforms()) ]
+        platform_name = 'CPU'
         for nonbonded_method in ['CutoffPeriodic', 'PME']:
             testsystem = testsystems.AlchemicalWaterBox(nonbondedMethod=getattr(app, nonbonded_method))
-            for platform_name in platform_names:
-                name = '%s %s %s' % (testsystem.name, nonbonded_method, platform_name)
-                self.compare_external_protocol_work_accumulation(testsystem, parameter_name, parameter_initial, parameter_final, platform_name=platform_name, name=name)
+            name = '%s %s %s' % (testsystem.name, nonbonded_method, platform_name)
+            self.compare_external_protocol_work_accumulation(testsystem, parameter_name, parameter_initial, parameter_final, platform_name=platform_name, name=name)
 
     def test_protocol_work_accumulation_waterbox_barostat(self, temperature=300*unit.kelvin):
         """
