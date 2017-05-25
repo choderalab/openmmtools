0e6c871fcaa7f49d2756bc463d76dfa02058ae67
Andrea Rizzi <andrrizzi@gmail.com>
2017-05-25 19:03:52 -0400

    Release 0.9.5

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ DOCLINES = __doc__.split("\n")
 
 ########################
 VERSION = "0.9.5"
-ISRELEASED = False
+ISRELEASED = True
 __version__ = VERSION
 ########################
 CLASSIFIERS = """\
