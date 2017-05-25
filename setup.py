570a92bcaabd4a0ba9b622a5f5e82af0b66a8583
Andrea Rizzi <andrrizzi@gmail.com>
2017-05-25 19:11:21 -0400

    Release 0.10.0

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ import subprocess
 DOCLINES = __doc__.split("\n")
 
 ########################
-VERSION = "0.9.5"
+VERSION = "0.10.0"
 ISRELEASED = True
 __version__ = VERSION
 ########################
