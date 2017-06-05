003a36dde8633f50e4e05f502035b965d3c12c37
Levi Naden <levi.naden@choderalab.org>
2017-06-05 13:12:02 -0400

    Once again try to fix the conda-build issue

--- devtools/travis-ci/install.sh
+++ devtools/travis-ci/install.sh
@@ -17,8 +17,7 @@ bash $MINICONDA -b -p $MINICONDA_HOME
 export PIP_ARGS="-U"
 export PATH=$MINICONDA_HOME/bin:$PATH
 conda config --add channels conda-forge
-conda update --yes conda\>=4.3
-conda install --yes conda-build jinja2 anaconda-client pip
+conda install --yes conda\>=4.3 conda-build jinja2 anaconda-client pip
 
 # Restore original directory
 popd
