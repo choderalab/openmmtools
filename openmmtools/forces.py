923d45daa7a734fd6f4f5be6e2c9a629a15345c7
Andrea Rizzi <andrrizzi@gmail.com>
2017-07-12 20:20:49 -0400

    Split replace_reaction_field into its own forcefactories module.

--- openmmtools/forces.py
+++ openmmtools/forces.py
@@ -14,8 +14,6 @@ Custom OpenMM Forces classes and utilities.
 # GLOBAL IMPORTS
 # =============================================================================
 
-import copy
-
 from simtk import openmm, unit
 
 from openmmtools.constants import ONE_4PI_EPS0
@@ -70,58 +68,6 @@ def iterate_nonbonded_forces(system):
     for force in system.getForces():
         if isinstance(force, openmm.NonbondedForce):
             yield force
-
-
-# =============================================================================
-# FACTORY FUNCTIONS
-# =============================================================================
-
-def replace_reaction_field(self, reference_system, switch_width=1.0*unit.angstrom,
-                           return_copy=True):
-    """Return a system converted to use a switched reaction-field electrostatics.
-
-    This will add an `UnshiftedReactionFieldForce` for each `NonbondedForce`
-    that utilizes `CutoffPeriodic`.
-
-    Note that `AbsoluteAlchemicalFactory.create_alchemical_system()` can NOT
-    handle the resulting `System` object yet since the `CustomNonbondedForce`
-    are not recognized and re-coded.
-
-    Parameters
-    ----------
-    reference_system : simtk.openmm.System
-        The system to use as a reference. This will be modified only if
-        `return_copy` is `False`.
-    switch_width : simtk.unit.Quantity, default 1.0*angstrom
-        Switch width for electrostatics (units of distance).
-    return_copy : bool
-        If `True`, `reference_system` is not modified, and a copy is returned.
-        Setting it to `False` speeds up the function execution but modifies
-        the `reference_system` object.
-
-    Returns
-    -------
-    system : simtk.openmm.System
-        System with reaction-field converted to c_rf = 0
-
-    """
-    if return_copy:
-        system = copy.deepcopy(reference_system)
-    else:
-        system = reference_system
-
-    # Add an UnshiftedReactionFieldForce for each CutoffPeriodic NonbondedForce.
-    for reference_force in iterate_nonbonded_forces(system):
-        if reference_force.getNonbondedMethod() == openmm.NonbondedForce.CutoffPeriodic:
-            reaction_field_force = UnshiftedReactionFieldForce.from_nonbonded_force(reference_force)
-            system.addForce(reaction_field_force)
-
-            # Remove particle electrostatics from reference force, but leave exceptions.
-            for particle_index in range(reference_force.getNumParticles()):
-                charge, sigma, epsilon = reference_force.getParticleParameters(particle_index)
-                reference_force.setParticleParameters(particle_index, abs(0.0*charge), sigma, epsilon)
-
-    return system
 
 
 # =============================================================================
