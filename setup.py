cf6afadd4cbbcfe1c5398a8746bf5819055503fd
Andrea Rizzi <andrrizzi@gmail.com>
2018-01-19 11:19:42 -0500

    Bump dev version to 0.14.0

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ import subprocess
 DOCLINES = __doc__.split("\n")
 
 ########################
-VERSION = "0.13.5"
+VERSION = "0.14.0"
 ISRELEASED = False
 __version__ = VERSION
 ########################
