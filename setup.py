2b6c060c62dd1ea881bd6ca32bacf87b17493285
Andrea Rizzi <andrea.rizzi@choderalab.org>
2018-03-22 22:40:28 -0400

    Bump to 0.15.1

--- setup.py
+++ setup.py
@@ -14,8 +14,8 @@ import subprocess
 DOCLINES = __doc__.split("\n")
 
 ########################
-VERSION = "0.15.0"
-ISRELEASED = True
+VERSION = "0.15.1"
+ISRELEASED = False
 __version__ = VERSION
 ########################
 CLASSIFIERS = """\
