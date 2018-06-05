475f6d3f99cd1556c5430251d7c2c9f5e3c6da72
Josh Fass <jf694@cornell.edu>
2018-06-05 17:21:42 -0400

    Remove unused reference to `MJHMCIntegrator`

--- openmmtools/hmc_integrators/__init__.py
+++ openmmtools/hmc_integrators/__init__.py
@@ -1,10 +1,8 @@
 from .ghmc import GHMCIntegrator
 from .xchmc import XCGHMCIntegrator, XCGHMCRESPAIntegrator
 from .ghmc_respa import RESPAMixIn, GHMCRESPAIntegrator, check_groups, guess_force_groups
-from .mjhmc import MJHMCIntegrator
 
 
 __all__ = ["GHMCIntegrator", "XCGHMCIntegrator", "XCGHMCRESPAIntegrator",
            "RESPAMixIn", "GHMCRESPAIntegrator", "check_groups", "guess_force_groups",
-           "MJHMCIntegrator",
            ]
