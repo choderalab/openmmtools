71b15ccd08a85b5d74160476183b15957987db23
John Chodera <john.chodera@choderalab.org>
2019-01-29 21:45:15 -0500

    Make sure to close storage on __del__ (port changes from https://github.com/choderalab/yank/pull/1135/)

--- openmmtools/multistate/multistatereporter.py
+++ openmmtools/multistate/multistatereporter.py
@@ -417,11 +417,8 @@ class MultiStateReporter(object):
 
     def __del__(self):
         """Synchronize and close the storage."""
-        for storage in self._storage:
-            if storage is not None:
-                self.close()
-                break
-
+        self.close()
+        
     def read_end_thermodynamic_states(self):
         """Read thermodynamic states at the ends of the protocol."
 
