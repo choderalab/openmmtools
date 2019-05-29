1e14040341c38bda3d5c253644ba9534895a0a75
Andrea Rizzi <andrrizzi@gmail.com>
2019-05-29 11:30:43 +0200

    Determine version in __init__ with versioneer

--- openmmtools/__init__.py
+++ openmmtools/__init__.py
@@ -6,3 +6,10 @@ Various Python utilities for OpenMM.
 """
 
 from openmmtools import testsystems, integrators, alchemy, mcmc, states, cache, utils, constants, forces, forcefactories, storage, multistate
+
+# Handle versioneer
+from ._version import get_versions
+versions = get_versions()
+__version__ = versions['version']
+__git_revision__ = versions['full-revisionid']
+del get_versions, versions
