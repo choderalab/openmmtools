030cc13bce3b8a6759fbb8f0a6d2b5fb9063e4c6
Andrea Rizzi <andrrizzi@gmail.com>
2019-05-29 11:26:40 +0200

    Let versioneer set the version

--- openmmtools/__init__.py
+++ openmmtools/__init__.py
@@ -5,8 +5,4 @@ Various Python utilities for OpenMM.
 
 """
 
-# Define global version.
-from openmmtools import version
-__version__ = version.version
-
 from openmmtools import testsystems, integrators, alchemy, mcmc, states, cache, utils, constants, forces, forcefactories, storage, multistate
