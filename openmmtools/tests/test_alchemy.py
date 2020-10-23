2f1883ae773cf11ecf4547d0e29ed001810e9152
John Chodera <john.chodera@choderalab.org>
2020-10-22 17:34:35 -0700

    Fix GBSA test

--- openmmtools/tests/test_alchemy.py
+++ openmmtools/tests/test_alchemy.py
@@ -882,8 +882,8 @@ def check_noninteracting_energy_components(reference_system, alchemical_system, 
                 # Add particle to Force..
                 parameters = reference_force.getParticleParameters(particle_index)
                 try:  # GBSAOBCForce
-                    force.addParticle(*parameters)
-                except NotImplementedError:  # CustomGBForce
+                    force.addParticle(*parameters)                    
+                except TypeError:  # CustomGBForce
                     force.addParticle(parameters)
 
         system.addForce(force)
@@ -1360,7 +1360,7 @@ class TestAbsoluteAlchemicalFactory(object):
         cls.test_regions['Toluene'] = AlchemicalRegion(alchemical_atoms=range(6))  # Only partially modified.
         cls.test_regions['AlanineDipeptide'] = AlchemicalRegion(alchemical_atoms=range(22))
         cls.test_regions['HostGuestExplicit'] = AlchemicalRegion(alchemical_atoms=range(126, 156))
-        cls.test_regions['TIP3P WaterBox'] = AlchemicalRegion(alchemical_atoms=range(0,3))        
+        cls.test_regions['TIP3P WaterBox'] = AlchemicalRegion(alchemical_atoms=range(0,3))
 
         # Modify ions.
         for atom in cls.test_systems['TIP4P-EW WaterBox and NaCl with PME'].topology.atoms():
