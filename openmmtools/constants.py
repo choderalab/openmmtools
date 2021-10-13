5870cc5463accf2ec5f21d2bb20ee8254e564cbf
Iván Pulido <ivanpulido@rotonmail.com>
2021-10-12 21:15:46 -0400

    Updating ONE_4PI_EPS0 to match CODATA 2018.

--- openmmtools/constants.py
+++ openmmtools/constants.py
@@ -1,3 +1,4 @@
+from math import pi
 import simtk.unit as u
 
 kB = u.BOLTZMANN_CONSTANT_kB * u.AVOGADRO_CONSTANT_NA
@@ -5,7 +6,9 @@ kB = u.BOLTZMANN_CONSTANT_kB * u.AVOGADRO_CONSTANT_NA
 # OpenMM constant for Coulomb interactions in OpenMM units
 # (openmm/platforms/reference/include/SimTKOpenMMRealType.h)
 # TODO: Replace this with an import from simtk.openmm.constants once available
-ONE_4PI_EPS0 = 138.935456
+E_CHARGE = 1.602176634e-19 * u.coulomb
+EPSILON0 = 8.8541878128e-12 * u.farad/u.meter
+ONE_4PI_EPS0 = 1e6*(u.AVOGADRO_CONSTANT_NA*E_CHARGE**2)/(4*pi*EPSILON0)
 
 # Standard-state volume for a single molecule in a box of size (1 L) / (avogadros number).
 LITER = 1000.0 * u.centimeters**3
