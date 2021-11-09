e63f3965f4f4ae84154939fa5f41b4545f89c27c
Iván Pulido <ivanpulido@protonmail.com>
2021-11-08 19:52:11 -0500

    Overriding deepcopy behavior in base integrator move class.

--- openmmtools/mcmc.py
+++ openmmtools/mcmc.py
@@ -762,6 +762,21 @@ class BaseIntegratorMove(MCMCMove):
         else:
             self.context_cache = utils.deserialize(serialization['context_cache'])
 
+    def __deepcopy__(self, memo):
+        """Overriding default deep copy behavior to avoid creating new ContextCache objects without need."""
+
+        if memo is None:
+            memo = {}
+
+        cls = self.__class__
+        new_state = cls.__new__(cls)
+        memo[id(self)] = new_state
+        for k, v in self.__dict__.items():
+            if k != 'context_cache':
+                new_state.__dict__[k] = copy.deepcopy(v, memo)
+        new_state.__dict__['context_cache'] = self.context_cache
+        return new_state
+
 
 # =============================================================================
 # METROPOLIZED MOVE BASE CLASS
@@ -1278,21 +1293,6 @@ class LangevinSplittingDynamicsMove(LangevinDynamicsMove):
                                               constraint_tolerance=self.constraint_tolerance,
                                               measure_shadow_work=self.measure_shadow_work,
                                               measure_heat=self.measure_heat)
-
-    def __deepcopy__(self, memo):
-        """Overriding default deep copy behavior to avoid creating new ContextCache objects without need."""
-
-        if memo is None:
-            memo = {}
-
-        cls = self.__class__
-        new_state = cls.__new__(cls)
-        memo[id(self)] = new_state
-        for k, v in self.__dict__.items():
-            if k != 'context_cache':
-                new_state.__dict__[k] = copy.deepcopy(v, memo)
-        new_state.__dict__['context_cache'] = self.context_cache
-        return new_state
 
 
 # =============================================================================
