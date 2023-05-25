d2d3035ed86ae359d2ab74f39c106b4e11855dcb
Iván Pulido <2949729+ijpulidos@users.noreply.github.com>
2023-05-25 14:30:54 -0400

    Importing everything in utils module

--- openmmtools/utils/__init__.py
+++ openmmtools/utils/__init__.py
@@ -7,27 +7,6 @@ Utility functions meant to be used in the different openmm modules or tests.
 This module provides a common API point for different utility functions that serve as convenience
 or help users in different miscellaneous tasks.
 """
-from .utils import (
-    _RESERVED_WORDS_PATTERNS,
-    deserialize,
-    find_all_subclasses,
-    get_available_platforms,
-    get_fastest_platform,
-    is_quantity_close,
-    platform_supports_precision,
-    quantity_from_string,
-    sanitize_expression,
-    math_eval,
-    RestorableOpenMMObject,
-    RestorableOpenMMObjectError,
-    serialize,
-    SubhookedABCMeta,
-    temporary_directory,
-    time_it,
-    Timer,
-    TrackedQuantity,
-    typename,
-    with_timer,
-)
+from .utils import *
 
 from .equilibration import run_gentle_equilibration
