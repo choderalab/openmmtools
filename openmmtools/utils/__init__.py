b7c9b6b19c5248abf2bd268c143d8ddb7406f612
Iván Pulido <2949729+ijpulidos@users.noreply.github.com>
2023-05-30 20:32:43 -0400

    Explicitly importing members of utils.py

--- openmmtools/utils/__init__.py
+++ openmmtools/utils/__init__.py
@@ -7,6 +7,34 @@ Utility functions meant to be used in the different openmm modules or tests.
 This module provides a common API point for different utility functions that serve as convenience
 or help users in different miscellaneous tasks.
 """
-from .utils import *
+from .utils import (
+    temporary_directory,
+    time_it,
+    with_timer,
+    Timer,
+    _RESERVED_WORDS_PATTERNS,
+    sanitize_expression,
+    math_eval,
+    _changes_state,
+    TrackedQuantity,
+    TrackedQuantityView,
+    _VALID_UNITS,
+    _VALID_UNIT_FUNCTIONS,
+    is_quantity_close,
+    quantity_from_string,
+    typename,
+    platform_supports_precision,
+    get_available_platforms,
+    get_fastest_platform,
+    _SERIALIZED_MANGLED_PREFIX,
+    serialize,
+    deserialize,
+    with_metaclass,
+    SubhookedABCMeta,
+    find_all_subclasses,
+    find_subclass,
+    RestorableOpenMMObjectError,
+    RestorableOpenMMObject
+)
 
 from .equilibration import run_gentle_equilibration
